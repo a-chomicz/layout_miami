d23a5ca97a90b4999379a91e206c28621e04a475
Aleksandra Chomicz <chomiczaa@gmail.com>
2025-08-08 12:07:20 +0200

    make build

---
+++ layout_miami.c40a92e8.js
@@ -0,0 +1,2 @@
+"use strict";window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),document.getElementById("form").addEventListener("submit",function(e){e.preventDefault(),this.reset()});
+//# sourceMappingURL=layout_miami.c40a92e8.js.map
